af7cc57b25a9fa516a0a7e2e1f39e36baed7e403
Pedro A. Chaves Jr. <pchavesjr@gmail.com>
2024-02-14 17:43:00 -0300

    Install LazyGit plugin

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,7 @@
+return {
+    "kdheepak/lazygit.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        vim.keymap.set('n', '<F7>', [[:LazyGit<CR>]], {})
+    end,
+}
