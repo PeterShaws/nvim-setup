964c5b73ddd4c1d8672d782be2837391287e51c5
Pedro A. Chaves Jr. <pchavesjr@gmail.com>
2024-02-16 11:42:21 -0300

    Install TreeSitter TextObjects plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
     build = ':TSUpdate',
     config = function()
         local configs = require('nvim-treesitter.configs')
@@ -19,6 +20,18 @@ return {
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ['af'] = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ['ac'] = '@class.outer',
+                        ['ic'] = '@class.inner',
+                    },
+                },
+            },
         })
     end,
 }
