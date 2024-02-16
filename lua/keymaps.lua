b384becee5a6af5e3d42390a4173902defb5ad80
Pedro A. Chaves Jr. <pchavesjr@gmail.com>
2024-02-16 11:06:54 -0300

    Add keymaps for quitting NeoVim

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,22 +5,24 @@ end
 
 --[[ Insert mode convenience ]]
 map('i', 'jk', '<esc>')      -- exit insert mode
-map('i', '<a-h>', '<left>')  -- move cursor left
-map('i', '<a-j>', '<down>')  -- move cursor down
-map('i', '<a-k>', '<up>')    -- move cursor up
-map('i', '<a-l>', '<right>') -- move cursor right
+map('i', '<A-h>', '<left>')  -- move cursor left
+map('i', '<A-j>', '<down>')  -- move cursor down
+map('i', '<A-k>', '<up>')    -- move cursor up
+map('i', '<A-l>', '<right>') -- move cursor right
 
 --[[ Window navigation ]]
-map('n', '<c-h>', '<c-w>h') -- focus window to the left
-map('n', '<c-j>', '<c-w>j') -- focus window below
-map('n', '<c-k>', '<c-w>k') -- focus window above
-map('n', '<c-l>', '<c-w>l') -- focus window to the right
-map('n', '<c-c>', '<c-w>c') -- close current window
+map('n', '<C-h>', '<C-w>h') -- focus window to the left
+map('n', '<C-j>', '<C-w>j') -- focus window below
+map('n', '<C-k>', '<C-w>k') -- focus window above
+map('n', '<C-l>', '<C-w>l') -- focus window to the right
+map('n', '<C-c>', '<C-w>c') -- close current window
 
 --[[ Miscellaneous ]]
 map('n', '<Leader>a', [[:Alpha<CR>]])    -- Invoke dashboard panel
+map('n', '<Leader>qq', [[:qa!<CR>]])     -- Quit NeoVim without saving anything
+map('n', '<Leader>qw', [[:wqa<CR>]])     -- Quit NeoVim, saving everything
 map('n', '<F5>', [[:luafile %<CR>]])     -- Source current lua file
-map('n', '<a-w>', [[:set wrap!]])        -- toggle word wrap
+map('n', '<A-w>', [[:set wrap!]])        -- toggle word wrap
 map('n', '<Leader>h', [[:noh<CR>]])      -- Clear search highlight
 map('n', '<Leader>Q', [[:%s/"/'/g<CR>]]) -- Replace all double quotes
 map('n', '<Leader>I', 'gg=G')            -- Reindent file
