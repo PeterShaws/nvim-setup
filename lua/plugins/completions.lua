bfec2ef5b2718400cc5f1bd7749b4cbfcfb72dd8
Pedro A. C. Jr. <inckie@gmail.com>
2024-02-19 11:08:57 -0300

    Update `nvim-cmp` config

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -38,8 +38,12 @@ return {
     {
         'hrsh7th/nvim-cmp',
         config = function()
-            local cmp = require('cmp')
             require('luasnip.loaders.from_vscode').lazy_load()
+
+            local cmp = require('cmp')
+            local luasnip = require('luasnip')
+
+            local select_opts = { behavior = cmp.SelectBehavior.Select }
 
             cmp.setup({
                 snippet = {
@@ -47,42 +51,74 @@ return {
                         require('luasnip').lsp_expand(args.body)
                     end,
                 },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                }),
                 sources = {
                     { name = 'path' },
                     { name = 'nvim_lsp', keyword_length = 1 },
                     { name = 'buffer',   keyword_length = 3 },
                     { name = 'luasnip',  keyword_length = 2 },
                 },
-                -- sources = cmp.config.sources({
-                --     { name = 'nvim_lsp' },
-                --     { name = 'luasnip' },
-                -- }, {
-                --     { name = 'buffer' },
-                -- }),
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
                 formatting = {
-                    format = function(entry, vim_item)
-                        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
-                        vim_item.menu = ({
-                            buffer = '[Buffer]',
-                            nvim_lsp = '[LSP]',
-                            luasnip = '[LuaSnip]',
-                            nvim_lua = '[Lua]',
-                            latex_symbols = '[LaTeX]',
-                        })[entry.source.name] or entry.source.name
-                        return vim_item
+                    fields = { 'menu', 'abbr', 'kind' },
+                    format = function(entry, item)
+                        local menu_icon = {
+                            nvim_lsp = '󰘧',
+                            nvim_lua = '',
+                            luasnip = '',
+                            buffer = '',
+                            path = '',
+                            latex_symbols = '',
+                        }
+                        item.kind = string.format('%s %s', kind_icons[item.kind], item.kind)
+                        item.menu = menu_icon[entry.source.name] or entry.source.name
+                        return item
                     end,
                 },
+                mapping = cmp.mapping.preset.insert({
+                    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+                    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+                    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+                    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-f>'] = cmp.mapping(function(fallback)
+                        if luasnip.jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-b>'] = cmp.mapping(function(fallback)
+                        if luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        local col = vim.fn.col('.') - 1
+                        if cmp.visible() then
+                            cmp.select_next_item(select_opts)
+                        elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                            fallback()
+                        else
+                            cmp.complete()
+                        end
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item(select_opts)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' })
+                }),
             })
 
             local highlight = vim.api.nvim_set_hl
