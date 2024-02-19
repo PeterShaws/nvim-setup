654651c062149b3f668ddbc02d3a4e65442b5684
Pedro A. C. Jr. <inckie@gmail.com>
2024-02-19 12:05:21 -0300

    Update keymaps
    - Refactor theme config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,9 @@
-local function map(mode, keys, target, options)
+local function map(mode, keys, command, options)
     options = options or {}
-    vim.api.nvim_set_keymap(mode, keys, target, options)
+    vim.keymap.set(mode, keys, command, options)
 end
 
---[[ Insert mode convenience ]]
+--[[ Insert mode navigation ]]
 map('i', '<A-h>', '<left>')  -- move cursor left
 map('i', '<A-j>', '<down>')  -- move cursor down
 map('i', '<A-k>', '<up>')    -- move cursor up
@@ -17,12 +17,34 @@ map('n', '<C-l>', '<C-w>l') -- focus window to the right
 map('n', '<C-c>', '<C-w>c') -- close current window
 
 --[[ Miscellaneous ]]
-map('n', '<Leader>a', [[:Alpha<CR>]])    -- Invoke dashboard panel
-map('n', '<Leader>cd', [[:cd %:h<CR>]])  -- Change to the current file's directory
-map('n', '<Leader>qq', [[:qa!<CR>]])     -- Quit NeoVim without saving anything
-map('n', '<Leader>qw', [[:wqa<CR>]])     -- Quit NeoVim, saving everything
-map('n', '<F5>', [[:luafile %<CR>]])     -- Source current lua file
-map('n', '<A-w>', [[:set wrap!<CR>]])    -- toggle word wrap
-map('n', '<Leader>h', [[:noh<CR>]])      -- Clear search highlight
-map('n', '<Leader>Q', [[:%s/"/'/g<CR>]]) -- Replace all double quotes
-map('n', '<Leader>I', 'gg=G')            -- Reindent file
+map('n', '<Leader>a', [[:Alpha<CR>]])                  -- Invoke dashboard panel
+map('n', '<Leader>cd', [[:cd %:h<CR>]])                -- Change to the current file's directory
+map('n', '<Leader>A', [[:keepjumps normal! ggVG<CR>]]) -- Select all text
+map({ 'n', 'x' }, 'gy', '"+y')                         -- Copy to clipboard
+map({ 'n', 'x' }, 'gp', '"+p')                         -- Paste from clipboard
+map('n', '<Leader>w', [[:write<CR>]])                  -- Write the current file
+map('n', '<Leader>qq', [[:qa!<CR>]])                   -- Quit NeoVim without saving anything
+map('n', '<Leader>qw', [[:wqa<CR>]])                   -- Quit NeoVim, saving everything
+map('n', '<F5>', [[:luafile %<CR>]])                   -- Source current lua file
+map('n', '<A-w>', [[:set wrap!<CR>]])                  -- Toggle word wrap
+map('n', '<Leader>h', [[:noh<CR>]])                    -- Clear search highlight
+map('n', '<Leader>Q', [[:%s/"/'/g<CR>]])               -- Replace all double quotes
+map('n', '<Leader>I', 'gg=G')                          -- Reindent file
+
+--[[ Autocommands ]]
+local augroup = vim.api.nvim_create_augroup('user_cmds', { clear = true })
+-- Quit help and man windows with just <q>
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'help', 'man' },
+    group = augroup,
+    desc = 'Use q to close the window',
+    command = 'nnoremap <buffer> q <cmd>quit<CR>',
+})
+-- Briefly highlight yanked text
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = augroup,
+    desc = 'Highlight on yank',
+    callback = function()
+        vim.highlight.on_yank({ higroup = 'Visual', timeout = 200 })
+    end,
+})
--- lua/plugins/github-nvim-theme.lua
+++ lua/plugins/github-nvim-theme.lua
@@ -3,7 +3,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd('colorscheme github_dark_default')
-        vim.cmd('hi ColorColumn guibg=#11151b')
+        vim.cmd.colorscheme('github_dark_default')
+        vim.cmd.highlight('ColorColumn guibg=#11151b')
     end,
 }
