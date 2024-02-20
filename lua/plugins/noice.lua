d7d5b4e6c40048769bd2b1ff1bfcd7d2ebd634e7
Pedro A. C. Jr. <inckie@gmail.com>
2024-02-20 00:18:50 -0300

    Install Noice plugin

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,39 @@
+return {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+        'MunifTanjim/nui.nvim',
+        'rcarriga/nvim-notify'
+    },
+    config = function()
+        local noice = require('noice')
+        noice.setup({
+            cmdline = {
+                format = {
+                    cmdline = { icon = '' },
+                    search_down = { icon = ' ' },
+                    search_up = { icon = ' ' },
+                    filter = { icon = '' },
+                    lua = { icon = '' },
+                    help = { icon = '󰘥' },
+                    input = {},
+                },
+            },
+            presets = {
+                command_palette = true,
+            },
+        })
+        require('lualine').setup({
+            sections = {
+                lualine_x = {
+                    {
+                        noice.api.statusline.mode.get,
+                        cond = noice.api.statusline.mode.has,
+                        color = { fg = '#ff9e64' }
+                    },
+                    'encoding', 'fileformat', 'filetype'
+                }
+            }
+        })
+    end,
+}
