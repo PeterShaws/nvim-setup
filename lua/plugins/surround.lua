4b8a73658b05d1adc2dc96d042a3dad27824fddc
Pedro A. C. Jr. <inckie@gmail.com>
2024-02-22 10:03:20 -0300

    Fix `surround` mappings

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,19 +1,19 @@
-local map = function(keys, handler, desc)
-    vim.keymap.set('n', keys, handler, { desc = desc })
+local map = function(mode, keys, handler, desc)
+    vim.keymap.set(mode, keys, handler, { desc = desc })
 end
 
 return {
     'tpope/vim-surround',
     config = function()
         -- Make Surround Leap-compatible
-        map('gz', [[<Plug>Ysurround]], 'Get Surround')                    -- ys
-        map('gZ', [[<Plug>YSurround]], 'Get Indented Surround')           -- yS
-        map('gzr', [[<Plug>Csurround]], 'Get Surround Replaced')          -- cs
-        map('gZr', [[<Plug>CSurround]], 'Get Indented Surround Replaced') -- cS
-        map('gzd', [[<Plug>Dsurround]], 'Get Surround Deleted')           -- ds
-        map('gzz', [[<Plug>Ysurround]], 'Get String Surround')            -- yss
-        map('gZZ', [[<Plug>YSurround]], 'Get Indented String Surround')   -- ySs
-        map('gz', [[<Plug>VSurround]], 'Visual Surround')                 -- S
-        map('gZ', [[<Plug>VgSurround]], 'Visual Indented Surround')       -- gS
+        map('n', 'gz', [[<Plug>Ysurround]], 'Get Surround')                    -- ys
+        map('n', 'gZ', [[<Plug>YSurround]], 'Get Indented Surround')           -- yS
+        map('n', 'gzr', [[<Plug>Csurround]], 'Get Surround Replaced')          -- cs
+        map('n', 'gZr', [[<Plug>CSurround]], 'Get Indented Surround Replaced') -- cS
+        map('n', 'gzd', [[<Plug>Dsurround]], 'Get Surround Deleted')           -- ds
+        map('n', 'gzz', [[<Plug>Ysurround]], 'Get String Surround')            -- yss
+        map('n', 'gZZ', [[<Plug>YSurround]], 'Get Indented String Surround')   -- ySs
+        map('v', 'gz', [[<Plug>VSurround]], 'Visual Surround')                 -- S
+        map('v', 'gZ', [[<Plug>VgSurround]], 'Visual Indented Surround')       -- gS
     end
 }
