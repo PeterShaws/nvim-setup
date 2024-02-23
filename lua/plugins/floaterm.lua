bb7d7759b358c872f1086a75b1db23d127e68a75
Pedro A. C. Jr. <inckie@gmail.com>
2024-02-23 15:06:05 -0300

    Update `floatterm` config

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,6 +1,15 @@
+local map = function(mode, keys, command, desc)
+    vim.keymap.set(mode, keys, command, { desc = desc, silent = true })
+end
+
 return {
     'voldikss/vim-floaterm',
     config = function()
-        vim.keymap.set('n', '<F12>', [[:FloatermToggle<CR>]], { desc = 'Open floating terminal' })
+        vim.g.floaterm_width = 0.92
+        vim.g.floaterm_height = 0.92
+        vim.g.floaterm_borderchars = '─│─│╭╮╯╰'
+
+        map('n', '<F12>', [[:FloatermToggle<CR>]], 'Toggle floating terminal')
+        map('t', '<F12>', [[:FloatermToggle<CR>]], 'Toggle floating terminal')
     end,
 }
