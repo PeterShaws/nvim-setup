510b074bbc9c61c97ec203d3cfbce61d9ce82296
Pedro A. C. Jr. <inckie@gmail.com>
2024-02-25 18:53:26 -0300

    Fix `huez` automatic colorscheme preview

--- lua/plugins/huez.lua
+++ lua/plugins/huez.lua
@@ -4,7 +4,10 @@ return {
     'vague2k/huez.nvim',
     dependencies = { 'nvim-telescope/telescope.nvim' },
     config = function()
-        require('huez').setup({})
+        require('huez').setup({
+            picker = 'telescope',
+            picker_opts = require('telescope.themes').get_dropdown({}),
+        })
 
         local colorscheme = require('huez.api').get_colorscheme()
         vim.cmd.colorscheme(colorscheme)
