a3654421c04a20420556b6483ebda9adeec2f07b
Pedro A. C. Jr. <inckie@gmail.com>
2024-02-28 14:51:54 -0300

    Install `cspell.nvim` plugin

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,6 @@
 return {
     'nvimtools/none-ls.nvim',
+    dependencies = { 'davidmh/cspell.nvim' },
     config = function()
         local null_ls = require('null-ls')
         null_ls.setup({
@@ -13,6 +14,7 @@ return {
                 null_ls.builtins.diagnostics.jsonls,
                 null_ls.builtins.diagnostics.markdownlint,
                 null_ls.builtins.diagnostics.pylint,
+                require('cspell').diagnostics,
             },
         })
     end,
