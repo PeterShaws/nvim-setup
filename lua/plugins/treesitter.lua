2ba5f8780e1a907f2dabd802360b1848e1931188
Pedro A. C. Jr. <inckie@gmail.com>
2024-03-04 15:23:23 -0300

    Add keymaps for Treesitter text objects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -25,6 +25,38 @@ return {
             highlight = { enable = true },
             indent = { enable = true },
             textobjects = {
+                move = {
+                    enable = true,
+                    set_jumps = true, -- whether to set jumps in the jumplist
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                        ["gj"] = "@function.outer",
+                        ["]]"] = "@class.outer",
+                        ["]b"] = "@block.outer",
+                        ["]a"] = "@parameter.inner",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                        ["gJ"] = "@function.outer",
+                        ["]["] = "@class.outer",
+                        ["]B"] = "@block.outer",
+                        ["]A"] = "@parameter.inner",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                        ["gk"] = "@function.outer",
+                        ["[["] = "@class.outer",
+                        ["[b"] = "@block.outer",
+                        ["[a"] = "@parameter.inner",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                        ["gK"] = "@function.outer",
+                        ["[]"] = "@class.outer",
+                        ["[B"] = "@block.outer",
+                        ["[A"] = "@parameter.inner",
+                    },
+                },
                 select = {
                     enable = true,
                     lookahead = true,
@@ -33,6 +65,14 @@ return {
                         ['if'] = '@function.inner',
                         ['ac'] = '@class.outer',
                         ['ic'] = '@class.inner',
+                        ['ab'] = '@block.outer',
+                        ['ib'] = '@block.inner',
+                        ['al'] = '@loop.outer',
+                        ['il'] = '@loop.inner',
+                        ['a/'] = '@comment.outer',
+                        ['i/'] = '@comment.outer',   -- no inner comment
+                        ['aa'] = '@parameter.outer', -- argument = parameter
+                        ['ia'] = '@parameter.inner',
                     },
                 },
             },
